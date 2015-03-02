5e19d7a8b085f0f851b29bd9df3b5857b7e44764
Zachary Neely <zrneely@mit.edu>
2015-03-02 15:43:12 -0500

    delete unused imports

--- Scavengr/app/src/main/java/mit/scavengr/CreateHuntActivity.java
+++ Scavengr/app/src/main/java/mit/scavengr/CreateHuntActivity.java
@@ -1,12 +1,9 @@
 package mit.scavengr;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
 public class CreateHuntActivity extends ActionBarActivity {
--- Scavengr/app/src/main/java/mit/scavengr/MainActivity.java
+++ Scavengr/app/src/main/java/mit/scavengr/MainActivity.java
@@ -1,15 +1,12 @@
 package mit.scavengr;
 
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
