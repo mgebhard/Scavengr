9c350d4f0929dfa96622f7136ccd3ea5f6893392
Megan Gebhard <gebhard@mit.edu>
2015-03-02 01:27:35 -0500

    Picture of City

--- Scavengr/app/build/generated/source/r/debug/mit/scavengr/R.java
+++ Scavengr/app/build/generated/source/r/debug/mit/scavengr/R.java
@@ -1299,7 +1299,8 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020030;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020031;
         public static final int abc_textfield_search_material=0x7f020032;
-        public static final int ic_launcher=0x7f020033;
+        public static final int boston=0x7f020033;
+        public static final int ic_launcher=0x7f020034;
     }
     public static final class id {
         public static final int action_bar=0x7f080031;
