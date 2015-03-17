7f8eb2576936b99eed82c500a3bc457ad2be6301
Zachary Neely <zrneely@mit.edu>
2015-03-17 11:14:56 -0400

    split into multiple files that make more sense, and fix a bug with reviews

--- ScavengrServer/lib/rest/reviews.js
+++ ScavengrServer/lib/rest/reviews.js
@@ -1,15 +1,17 @@
-Router.route('/users', function() {
+Router.route('/reviews/', function() {
     this.response.statusCode = 200;
     this.response.setHeader("Content-Type", "application/json");
     this.response.setHeader("Access-Control-Allow-Origin", "*");
     this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     if(this.request.method == 'GET') {
-        this.response.end(JSON.stringify(Users.find({}, { _id: 1 }).fetch().map(function(x) { return { id: x._id._str, name: x.name }})));
+        this.response.end(JSON.stringify(
+            Reviews.find({}, { _id: 1 }).fetch().map(function(x) { return { id: x._id._str }; })
+        ));
     } else if(this.request.method == 'POST') {
-        this.response.end(JSON.stringify({ id: Users.insert(this.request.body)._str }));
+        this.response.end(JSON.stringify({ id: Reviews.insert(this.request.body)._str }));
     } else if(this.request.method == 'OPTIONS') {
-        this.response.setHeader('Access-Control-Allow-Methods', "POST, GET, OPTIONS");
+        this.response.setHeader('Access-Control-Allow-Methods', "GET, POST, OPTIONS");
         this.response.end("OPTIONS Response");
     } else {
         this.response.statusCode = 405;
@@ -17,14 +19,14 @@ Router.route('/users', function() {
     }
 }, { where: 'server' });
 
-Router.route('/users/byName/:name', function() {
+Router.route('/reviews/byAuthor/:author', function() {
     this.response.statusCode = 200;
     this.response.setHeader("Content-Type", "application/json");
     this.response.setHeader("Access-Control-Allow-Origin", "*");
     this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     if(this.request.method == 'GET') {
-        this.response.end(JSON.stringify(Users.find({ name: this.params.name }).map(function(x) {
+        this.response.end(JSON.stringify(Users.find({ author: this.params.author }).map(function(x) {
             return { id: x._id._str };
         })));
     } else if(this.request.method == 'OPTIONS') {
@@ -36,24 +38,25 @@ Router.route('/users/byName/:name', function() {
     }
 }, { where: 'server' });
 
-Router.route('/users/:userId', function() {
+Router.route('/reviews/:reviewId', function() {
     this.response.statusCode = 200;
     this.response.setHeader("Content-Type", "application/json");
     this.response.setHeader("Access-Control-Allow-Origin", "*");
     this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     if(this.request.method == 'GET') {
-        this.response.end(JSON.stringify(Users.findOne({ _id: new Meteor.Collection.ObjectID(this.params.userId) })));
+        this.response.end(JSON.stringify(Reviews.findOne({ _id: new Meteor.Collection.ObjectID(this.params.reviewId) })));
     } else if(this.request.method == 'PUT') {
-        this.response.end(JSON.stringify(Users.update({ _id: new Meteor.Collection.ObjectID(this.params.userId) },
+        this.response.end(JSON.stringify(Reviews.update({ _id: new Meteor.Collection.ObjectID(this.params.reviewId) },
                 { $set: this.request.body })));
     } else if(this.request.method == 'DELETE') {
-        this.response.end(JSON.stringify(Users.remove({ _id: new Meteor.Collection.ObjectID(this.params.userId) })));
+        this.response.end(JSON.stringify(Reviews.remove({ _id: new Meteor.Collection.ObjectID(this.params.reviewId) })));
     } else if(this.request.method == 'OPTIONS') {
         this.response.setHeader('Access-Control-Allow-Methods', "PUT, GET, DELETE, OPTIONS");
         this.response.end("OPTIONS Response");
     } else {
-        this.responseCode = 405;
+        this.response.statusCode = 405;
         this.response.end("Not Allowed");
     }
+
 }, { where: 'server' });
