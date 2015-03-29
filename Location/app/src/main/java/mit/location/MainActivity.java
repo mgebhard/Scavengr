10f321c3e8c9eec21e898def65770a27abd87f02
Megan Gebhard <gebhard@mit.edu>
2015-03-29 17:53:21 -0400

    Location changes

--- Location/app/src/main/java/mit/location/MainActivity.java
+++ Location/app/src/main/java/mit/location/MainActivity.java
@@ -1,11 +1,13 @@
 package mit.location;
 
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.TrafficStats;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,7 +15,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -24,8 +30,14 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         LocationListener {
 
     LocationManager locationManager;
+    public File root = android.os.Environment.getExternalStorageDirectory();
+    public File locationLog = new File(root.getAbsolutePath(), "locationLog.txt");
+    public FileOutputStream fos;
+    public PrintWriter pw;
+    public Boolean isLocationServiceOn = false;
     public final static int REQUEST_LOCATION_UPDATE_TIMER = 5*60*1000;
     public final static int REQUEST_LOCATION_UPDATE_MINDISTANCE_METER = 500;
+
 
     private static final String REMOTE_LOCATION =
             "http://web.mit.edu/21w.789/www/papers/griswold2004.pdf";
@@ -35,7 +47,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.location:
                 // call method to get lat/long and acc
-                logLatLongAcc();
+                toggleLocationService();
                 break;
             case R.id.download:
                 // call download on PDF
@@ -46,8 +58,31 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
     }
 
-    public void logLatLongAcc() {
 
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void toggleLocationService() {
+        if (isLocationServiceOn) {
+            locationManager.removeUpdates(this);
+            pw.flush();
+            pw.close();
+            try {
+                fos.close();
+            } catch (IOException e) {
+
+            }
+
+        } else{
+            startLocationUpdates();
+        }
+        isLocationServiceOn = !isLocationServiceOn;
 
     }
 
@@ -94,11 +129,21 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         //get location service
         locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    public void startLocationUpdates() {
+        Criteria cr = new Criteria();
         locationManager.requestLocationUpdates(
-                LocationManager.NETWORK_PROVIDER, // GPS_PROVIDER
+                locationManager.getBestProvider(cr, true), // GPS_PROVIDER
                 REQUEST_LOCATION_UPDATE_TIMER, // 5*60*1000
                 REQUEST_LOCATION_UPDATE_MINDISTANCE_METER, // 500
                 this);
+        try {
+            fos = new FileOutputStream(locationLog);
+            pw = new PrintWriter(fos);
+        } catch (FileNotFoundException e) {
+
+        }
     }
 
 
@@ -131,6 +176,12 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         String provider = location.getProvider();
         float accuracy = location.getAccuracy();
         float speed = location.getSpeed();
+
+        if (isExternalStorageWritable()) {
+            pw.printf("TimeStamp: %d, Lat: %d, Long: %d, Provider: %s, Accuracy: %f, Speed %f \n",
+                    System.currentTimeMillis(), lat, lng, provider, accuracy, speed);
+        }
+
 
     }
 
