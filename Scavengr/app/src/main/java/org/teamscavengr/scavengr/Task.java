c332d1f6fbda5fd8bfb25ff8e0121ae80e01b33b
Helen Zhou <hlzhou@mit.edu>
2015-04-04 19:38:55 -0400

    changes to Task

--- Scavengr/app/src/main/java/org/teamscavengr/scavengr/Task.java
+++ Scavengr/app/src/main/java/org/teamscavengr/scavengr/Task.java
@@ -2,6 +2,8 @@ package org.teamscavengr.scavengr;
 
 import android.location.Location;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -12,24 +14,26 @@ public class Task {
 
     private final String id;
     private final Location location;
-    private final String text;
+    private final String clue;
     private final double radius; // in meters
+    private final String answer;
 
     public Task(JSONObject obj) throws JSONException {
         this.id = obj.getJSONObject("_id").getString("_str");
         this.location = new Location(""); // no provider
         this.location.setLatitude(Double.parseDouble(obj.getString("latitude")));
         this.location.setLongitude(Double.parseDouble(obj.getString("longitude")));
-        this.text = obj.getString("text");
+        this.clue = obj.getString("clue");
         this.radius = Double.parseDouble(obj.getString("radius"));
+        this.answer = obj.getString("answer");
     }
 
     public String getId() {
         return id;
     }
 
-    public String getText() {
-        return text;
+    public String getClue() {
+        return clue;
     }
 
     public double getRadius() {
@@ -38,5 +42,11 @@ public class Task {
 
     public Location getLocation() {
         return location;
+    }
+
+    public String getAnswer() { return answer; }
+
+    public double distanceFrom(LatLng pointFrom) {
+        return CalcLib.distanceFromLatLng(new LatLng(this.location.getLatitude(), this.location.getLongitude()), pointFrom);
     }
 }
