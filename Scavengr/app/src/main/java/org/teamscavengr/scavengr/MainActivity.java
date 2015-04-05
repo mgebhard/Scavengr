63faf599ea7dbc558476205abd5e7da247943158
Zachary Neely <zrneely@mit.edu>
2015-04-05 03:35:59 -0400

    GEOFENCING XD

--- Scavengr/app/src/main/java/org/teamscavengr/scavengr/MainActivity.java
+++ Scavengr/app/src/main/java/org/teamscavengr/scavengr/MainActivity.java
@@ -1,22 +1,61 @@
 package org.teamscavengr.scavengr;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.LocationServices;
 
 import org.teamscavengr.scavengr.createhunt.MyHuntsActivity;
 import org.teamscavengr.scavengr.goonhunt.HuntsList;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        ResultCallback<Status> {
+
+    private GoogleApiClient googleApiClient;
+    private GeofenceManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buildApiClient();
+
+        manager = new GeofenceManager(this, googleApiClient);
+    }
+
+    private synchronized void buildApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
     }
 
     @Override
@@ -70,9 +109,49 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                 Intent createHuntIntent = new Intent(this, MyHuntsActivity.class);
                 this.startActivity(createHuntIntent);
                 break;
+            case R.id.geofenceButton:
+                if(!googleApiClient.isConnected()) {
+                    Toast.makeText(this, "client not connected", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                Location l = new Location("");
+                l.setLatitude(10d);
+                l.setLongitude(10d);
+                manager.addGeofence("testGeofenceInNigeria", l, 1000, Geofence.NEVER_EXPIRE, this,
+                        new GeofenceManager.GeofenceListener() {
+                            @Override
+                            public void geofenceTriggered(final GeofenceManager.GeofenceEvent event) {
+                                Log.d("SCV", event.geofenceId);
+                            }
+                        });
             default:
                 break;
         }
 
+    }
+
+    @Override
+    public void onConnected(final Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(final int i) {
+        // Reconnect?
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(final ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onResult(final Status status) {
+        if(status.isSuccess()) {
+            Toast.makeText(this, "W00T", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Awwww... :(", Toast.LENGTH_SHORT).show();
+        }
     }
 }
