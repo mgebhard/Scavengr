460d3bc3b28f81f1ea1bf7f8ce3d227fb6ae7e11
Everardo Rosales <erosales@mit.edu>
2015-04-04 20:02:12 -0400

    List adapter progress.

--- Scavengr/app/src/main/java/org/teamscavengr/scavengr/goonhunt/HuntsList.java
+++ Scavengr/app/src/main/java/org/teamscavengr/scavengr/goonhunt/HuntsList.java
@@ -2,6 +2,7 @@ package org.teamscavengr.scavengr.goonhunt;
 
 import android.app.ListActivity;
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
@@ -17,6 +18,7 @@ import android.widget.LinearLayout.LayoutParams;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -29,14 +31,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 
-public class HuntsList extends ListActivity implements
-        LoaderManager.LoaderCallbacks<Cursor> {
+public class HuntsList extends ListActivity {
 
     // This is the Adapter being used to display the list's data cursor -> XML fields
-    SimpleCursorAdapter mAdapter;
+    ArrayAdapter mAdapter;
+    ArrayList<String> mHuntNames = new ArrayList<String>();
 
     // These are the Hunt rows that we will retrieve from DB
-//    static final String[] PROJECTION = new String[] {HuntContract.Data._ID,
+    //static final String[] PROJECTION = new String[] {HuntContract.Data._ID,
 //            HuntContract.Data.DISPLAY_NAME};
 
     // This is the select criteria pass in geo location
@@ -54,8 +56,8 @@ public class HuntsList extends ListActivity implements
                 LayoutParams.WRAP_CONTENT, Gravity.CENTER));
         progressBar.setIndeterminate(true);
         getListView().setEmptyView(progressBar);
-//
-//        // Must add the progress bar to the root of the layout
+
+        // Must add the progress bar to the root of the layout
         ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
         root.addView(progressBar);
 
@@ -68,51 +70,60 @@ public class HuntsList extends ListActivity implements
 
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, values);
-        setListAdapter(adapter);
-        */
+                android.R.layout.simple_list_item_1, values); */
+
+
 
 
         // For the cursor adapter, specify which columns go into which views
         // use static hunt objects for now
-        String[] reviewId = {"12345643210"};
+        /*String[] reviewId = {"12345643210"};
         Task[] tasks = new Task[1];
         try {
             tasks[0] = new Task(new JSONObject(""));
         } catch (JSONException e) {
 
         }
-        Hunt basicHunt = new Hunt("basicHunt", "1234567890", reviewId, tasks);
+        Hunt basicHunt = new Hunt("basicHunt", "1234567890", reviewId, tasks);*/
         ArrayList<Hunt> hunts = new ArrayList<Hunt>();
         Hunt.loadAllHuntsInBackground(
             new Hunt.HuntLoadedCallback() {
 
                    @Override
                    public void numHuntsFound(int num) {
-                     
+                       Context context = getApplicationContext();
+                       CharSequence text = "Loading " + num + " hunts...";
+                       int duration = Toast.LENGTH_LONG;
+
+                       Toast toast = Toast.makeText(context, text, duration);
                    }
 
                    @Override
                    public void huntLoaded(Hunt hunt) {
-                        //hunts
+                       mHuntNames.add(hunt.getName());
+                       mAdapter.notifyDataSetChanged();
+
                    }
 
                    @Override
                    public void huntFailedToLoad(Exception e) {
+                       Context context = getApplicationContext();
+                       CharSequence text = "Failed to load a hunt";
+                       int duration = Toast.LENGTH_SHORT;
 
+                       Toast toast = Toast.makeText(context, text, duration);
                    }
                }, true);
-        hunts.add(basicHunt);
-
-        String[] fromColumns = {hunts[0].getId() , hunts[0].getName()};
+        //hunts.add(basicHunt);
+        mHuntNames.add("Loading Hunts");
+        //String[] fromColumns = {hunts.get(0).getId() , hunts.get(0).getName()};
         //String[] fromColumns = {ContactsContract.Data.DISPLAY_NAME};
         int[] toViews = {R.id.hunt_icon, R.id.hunt_label}; // The TextView in simple_list_item_1
 
         // Create an empty adapter we will use to display the loaded data.
         // We pass null for the cursor, then update it in onLoadFinished()
-        mAdapter = new SimpleCursorAdapter(this,
-                R.layout.hunt_list_row, null,
-                fromColumns, toViews, 0);
+        mAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, mHuntNames);
         setListAdapter(mAdapter);
 
 
@@ -122,7 +133,7 @@ public class HuntsList extends ListActivity implements
     }
 
     // Called when a new Loader needs to be created
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+    /*public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         // Now create and return a CursorLoader that will take care of
         // creating a Cursor for the data being displayed.
 //        return new CursorLoader(this, ContactsContract.Data.CONTENT_URI,
@@ -143,7 +154,7 @@ public class HuntsList extends ListActivity implements
         // above is about to be closed.  We need to make sure we are no
         // longer using it.
         mAdapter.swapCursor(null);
-    }
+    }*/
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
