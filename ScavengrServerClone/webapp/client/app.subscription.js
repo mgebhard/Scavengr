fb06b11c0475ce1708503b9862474eb33efb4125
Zachary Neely <zrneely@mit.edu>
2015-04-06 00:01:37 -0400

    remove clone

--- ScavengrServerClone/webapp/client/app.subscription.js
+++
@@ -1,7 +0,0 @@
-Deps.autorun(function(){
-  Meteor.subscribe ('statistics');
-  Meteor.subscribe ('posts');
-  Meteor.subscribe ('users');
-  Meteor.subscribe ('reviews');
-  Meteor.subscribe ('hunts');
-});
