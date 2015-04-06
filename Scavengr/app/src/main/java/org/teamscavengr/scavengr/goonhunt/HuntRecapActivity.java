c669aa0375cc28695f8a129f667268f7364d5db2
Megan Gebhard <gebhard@mit.edu>
2015-04-05 21:48:12 -0400

    git status :

--- Scavengr/app/src/main/java/org/teamscavengr/scavengr/goonhunt/HuntRecapActivity.java
+++ Scavengr/app/src/main/java/org/teamscavengr/scavengr/goonhunt/HuntRecapActivity.java
@@ -2,29 +2,37 @@ package org.teamscavengr.scavengr.goonhunt;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import org.teamscavengr.scavengr.BaseActivity;
+import org.teamscavengr.scavengr.Hunt;
 import org.teamscavengr.scavengr.MainActivity;
 import org.teamscavengr.scavengr.R;
 
 
 public class HuntRecapActivity extends BaseActivity implements View.OnClickListener {
 
+    private Hunt hunt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hunt_recap);
+        if (getIntent().hasExtra("huntObject")) {
+            hunt = (getIntent().getParcelableExtra("huntObject"));
+        }
     }
 
     public void onClick(View view) {
         switch(view.getId()) {
             case R.id.review:
-                Intent hunt = new Intent(this, RateHuntActivity.class);
-                this.startActivity(hunt);
+                Intent review = new Intent(this, RateHuntActivity.class);
+                review.putExtra("huntObj", (Parcelable)hunt);
+                this.startActivity(review);
                 break;
             case R.id.home:
                 Intent createHuntIntent = new Intent(this, MainActivity.class);
--- Scavengr/app/src/main/java/org/teamscavengr/scavengr/goonhunt/RateHuntActivity.java
+++ Scavengr/app/src/main/java/org/teamscavengr/scavengr/goonhunt/RateHuntActivity.java
@@ -6,25 +6,43 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import org.teamscavengr.scavengr.BaseActivity;
+import org.teamscavengr.scavengr.Hunt;
 import org.teamscavengr.scavengr.R;
+import org.teamscavengr.scavengr.Review;
 
 
 public class RateHuntActivity extends BaseActivity implements View.OnClickListener {
 
+    private Hunt hunt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_hunt);
+        if (getIntent().hasExtra("huntObject")) {
+            hunt = (getIntent().getParcelableExtra("huntObject"));
+        }
     }
 
     public void onClick(View view) {
         switch(view.getId()) {
             case R.id.submit_review:
                 Intent recap = new Intent(this, HuntRecapActivity.class);
+                RatingBar reviewRating = (RatingBar) findViewById(R.id.reviewRatingBar);
+                TextView reviewComments = (TextView) findViewById(R.id.reviewComments);
+
+                Review currentReview = new Review(null, hunt.getId(),
+                        reviewRating.getRating(), reviewComments.getText().toString());
+
+                // TODO(ZACH || EVER): save the review to the DB
+
                 this.startActivity(recap);
                 break;
+
             default:
                 break;
         }
