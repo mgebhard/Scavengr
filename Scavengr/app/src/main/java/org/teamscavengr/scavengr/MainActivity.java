abea7ab96404e228c13cb5bc9d730fe2098423b8
Helen Zhou <hlzhou@mit.edu>
2015-04-07 14:17:22 -0400

    myhunts home

--- Scavengr/app/src/main/java/org/teamscavengr/scavengr/MainActivity.java
+++ Scavengr/app/src/main/java/org/teamscavengr/scavengr/MainActivity.java
@@ -8,6 +8,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
