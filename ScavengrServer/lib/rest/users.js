f0b0719fe8425b9b81912b78a35f5a3467845f9f
Everardo Rosales <erosales@mit.edu>
2015-04-20 16:46:20 -0400

    Added server functionality for users on the server side.

--- ScavengrServer/lib/rest/users.js
+++ ScavengrServer/lib/rest/users.js
@@ -59,3 +59,65 @@ Router.route('/users/:userId', function() {
         this.response.end("Not Allowed");
     }
 }, { where: 'server' });
+
+Router.route('/users/:userId/reviews/', function() {
+    this.response.statusCode = 200;
+    this.response.setHeader("Content-Type", "application/json");
+    this.response.setHeader("Access-Control-Allow-Origin", "*");
+    this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    if(this.request.method == 'GET') {
+        this.response.end(JSON.stringify(
+            Users.findOne({ _id: new Meteor.Collection.ObjectID(this.params.userId) })['reviews']
+        ));
+    } else if(this.request.method == 'DELETE') {
+        Users.update({ _id: new Meteor.Collection.ObjectID(this.params.userId) },
+            { $pull: {
+                reviews: { id: this.request.body.id }
+            }});
+        this.response.end(JSON.stringify({}));
+    } else if(this.request.method == 'POST') {
+        this.response.end(JSON.stringify(Users.update({ _id: new Meteor.Collection.ObjectID(this.params.userId) },
+                { $push: {
+                    reviews: { id: this.request.body.id }
+                }})));
+    } else if(this.request.method == 'OPTIONS') {
+        this.response.setHeader('Access-Control-Allow-Methods', "POST, GET, DELETE, OPTIONS");
+        this.response.end("OPTIONS Response");
+    } else {
+        this.response.statusCode = 405;
+        this.response.end("Not Allowed");
+    }
+}, { where: 'server' });
+
+Router.route('/users/:userId/hunts/:huntId/', function() {
+    this.response.statusCode = 200;
+    this.response.setHeader("Content-Type", "application/json");
+    this.response.setHeader("Access-Control-Allow-Origin", "*");
+    this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    var taskId = this.params.taskId;
+
+    if(this.request.method == 'GET') {
+        this.response.end(JSON.stringify(
+                Users.findOne({
+                    _id: new Meteor.Collection.ObjectID(this.params.userId),
+                    'hunts._id': new Meteor.Collection.ObjectID(this.params.userId)
+                })['hunts'].filter(function(task) {
+                    return hunts._id._str == huntId;
+                })[0]
+        ));
+    } else if(this.request.method == 'DELETE') {
+        Users.update({ _id: new Meteor.Collection.ObjectID(this.params.huntId) },
+            { $pull: {
+                hunts: { _id: new Meteor.Collection.ObjectID(this.params.huntId) }
+            }});
+        this.response.end(JSON.stringify({}));
+    } else if(this.request.method == 'OPTIONS') {
+        this.response.setHeader('Access-Control-Allow-Methods', "GET, DELETE, OPTIONS");
+        this.response.end("OPTIONS Response");
+    } else {
+        this.response.statusCode = 405;
+        this.response.end("Not Allowed");
+    }
+}, { where: 'server' });
